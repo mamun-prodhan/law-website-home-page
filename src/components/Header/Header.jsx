e32cafaa230555169dd69014482117c7c0e99f4b
mamun-prodhan <mamunpr9@gmail.com>
2023-08-05 01:50:56 +0600

    banner bug fixe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
     <div>
       {/* image div */}
       <div className="w-full bg-[#1B2946]">
-        <div className="ps-0 md:ps-14 lg:ps-14 pt-8 md:pt-14 lg:pt-14 pb-3 ">
-          <img className="mx-auto md:mx-0 lg:mx-0" src={logo} alt="logo" />
+        <div className="ps-0 md:ps-10 lg:ps-14 pt-8 md:pt-10 lg:pt-14 pb-3 ">
+          <img className="mx-auto md:mx-0 lg:mx-0 w-[114px] md:w-[144px] lg:w-[154px]" src={logo} alt="logo" />
         </div>
         <div>
           <img className="w-[100%]" src={banner} alt="banner" />
